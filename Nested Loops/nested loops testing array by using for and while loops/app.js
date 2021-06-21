c2a8c93794194e43722b98badbdc71f72700e9bc
unknown <ssyedkhalil00@gmail.com>
2021-06-21 14:47:14 +0500

    Nested loop

---
+++ Nested Loops/nested loops testing array by using for and while loops/app.js
@@ -0,0 +1,53 @@
+let arr=[
+     [1, 2],
+     [3, 4],
+     [5, 6],
+
+    ];
+  
+    for (let i = 0; i < arr.length; i++) {
+        for (let k = 0; k < arr[i].length; k++) {
+            console.log(arr[i][k]);
+            
+        }
+        
+    }
+    let arr1=[
+        [1, 2],
+        [3, 4],
+        [5, 6],
+   
+       ];
+let i=0;
+      while (i<arr1.length) {
+          let j=0;
+          while (j < arr1[i].length) {
+              console.log(arr1[i][j])
+              j++
+          }
+          i++
+      }
+
+
+
+
+
+
+    let array= [
+
+        ["cat" , "rabbit"],
+        ["pigeon" , "parrot"],
+        ["goldfish" , "whale"],
+         
+    ];
+
+    for (let i = 0; i < array.length; i++) {
+        for (let c = 0; c < array[i].length; c++) {
+            console.log(array[i][c])
+            if (condition) {
+                
+            }
+            
+        }
+        
+    }
